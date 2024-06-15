70c0ca49028ef915f5dd174e750be4be2dd1f940
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2024-06-15 02:53:54 +0100

    Update Rate and Pitch settings: use percentage instead of prefixed values

--- assets/components/TemporaryDrawer.tsx
+++ assets/components/TemporaryDrawer.tsx
@@ -48,15 +48,14 @@ export default function TemporaryDrawer(props: any) {
             </IconButton>
                 <Typography sx={{ position: 'fixed', top: '6%', left: '50%', transform: 'translate(-50%, -50%)' }} variant='h6'>Additional Settings</Typography>
             <Stack spacing={2} sx={{ mx: 2 }}>
-                <CustomSlider value={settings.rate} labels={['default', 'x-slow', 'slow', 'medium', 'fast', 'x-fast']} label='Rate' onChange={(e: any, value: number) => handleSliderChange(value, 'set_rate')} />
-                <CustomSlider value={settings.pitch} labels={['default', 'x-low', 'low', 'medium', 'high', 'x-high']} label='Pitch' onChange={(e: any, value: number) => handleSliderChange(value, 'set_pitch')} />
+                <CustomSlider value={settings.rate} labels={['Slow', 'Default', 'Fast']} min={-50} max={50} defaultValue={0} label='Rate' onChange={(e: any, value: number) => handleSliderChange(value, 'set_rate')} />
+                <CustomSlider value={settings.pitch} labels={['Low', 'Default', 'High']} min={-50} max={50} defaultValue={0} label='Pitch' onChange={(e: any, value: number) => handleSliderChange(value, 'set_pitch')} />
             </Stack>
             <Divider sx={{ m: 2 }} />
             {
                 import.meta.env.CHROME &&
                 <>
                     <FormControlLabel sx={{ m: 1 }} control={<Switch checked={isSupportDev} onChange={handleSwitchChange} />} label="Support Developer ☕" />
-                    {/* @ts-expect-error */}
                     <Typography m={2}>Read more <Link href={browser.runtime.getURL('/options.html')} target="_blank">here</Link></Typography>
                     <Divider sx={{ m: 2 }} />
                 </>
